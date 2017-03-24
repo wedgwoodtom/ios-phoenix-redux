6b092a3c58dca9e32abc698aa2f348662e217d35
Tom Patterson <tom.patterson@comcast.com>
2017-03-23 17:43:54 -0700

    Abstract this stuff a bit

---
+++ PhoenixRedux/HeadsUpDisplay.swift
@@ -0,0 +1,95 @@
+//
+// Created by Tom Patterson on 3/23/17.
+// Copyright (c) 2017 Tom Patterson. All rights reserved.
+//
+
+import Foundation
+
+import SpriteKit
+
+class HeadsUpDisplay {
+
+    private var score: Int = 0
+    var scoreNode = SKLabelNode()
+    private var shipsLeftList = Array<SKSpriteNode>()
+    // heads up display
+    private var hud = SKSpriteNode()
+
+    public func init() {
+        createHUD()
+    }
+
+    func SKSpriteNode() -> SKSpriteNode {
+        return hud
+    }
+
+    func updateHUDForScoreDecrement() {
+        score -= 200
+        self.scoreNode.text = "\(score)"
+    }
+
+    func updateHUDForScore() {
+        score += 100
+        self.scoreNode.text = "\(score)"
+    }
+
+    func updateHUDForShipDestroyed() {
+        if shipsLeftList.count == 0 {
+            return
+        }
+
+        let last: SKSpriteNode = shipsLeftList.removeLast()
+        last.run(SKAction.sequence([SKAction.fadeOut(withDuration: 1.5), SKAction.removeFromParent()]))
+        // TODO: below should not be necessary.  However, without it, the lives are not drawn on the iPhone although it works as expected in the simulator
+        drawRemaingLives()
+    }
+
+    func resetHUD() {
+        score = 0;
+        self.scoreNode.text = "\(score)"
+        drawRemaingLives()
+    }
+
+    func drawRemaingLives() {
+        // reset lives left
+        let lifeSize = CGSize(width: hud.size.height-18, height: hud.size.height-18)
+        hud.removeChildren(in: shipsLeftList)
+        shipsLeftList.removeAll()
+
+        for i in 0..<shipsLeft-1 {
+            let tmpNode = SKSpriteNode(imageNamed: "Spaceship.png")
+            shipsLeftList.append(tmpNode)
+            tmpNode.size = lifeSize
+            tmpNode.position=CGPoint(x: tmpNode.size.width * 1.3 * (1.0 + CGFloat(i)), y: (hud.size.height-5)/2)
+            tmpNode.name = "shipLifeIcon"
+            hud.addChild(tmpNode)
+        }
+    }
+
+    func createHUD() {
+        //hud.color = .black
+        hud.size = CGSize(width: self.size.width, height: self.size.height * 0.05)
+
+        hud.anchorPoint = CGPoint(x: 0, y: 0)
+        hud.position = CGPoint(x: 0, y: self.size.height-hud.size.height)
+//        hud.position = CGPoint(x: 0, y: hud.size.height)
+        // hack for iPad - how to fix this?
+        if (UIDevice.current.userInterfaceIdiom == .pad) {
+            hud.position.y -= 175
+        }
+        self.addChild(hud)
+
+        // Display the current score
+        self.score = 0
+//        self.scoreNode.position = CGPoint(x: hud.size.width-hud.size.width * 0.1, y: 1)
+        self.scoreNode.position = CGPoint(x: hud.size.width-hud.size.width * 0.1, y: 20)
+        self.scoreNode.text = ""
+        //self.scoreNode.fontSize = hud.size.height * 0.50
+        self.scoreNode.fontName = "Helvetica Neue Medium"
+        self.scoreNode.fontSize = 30
+        //self.scoreNode.font = UIFont.boldSystemFontOfSize(hud.size.height * 0.50)
+        self.scoreNode.fontColor = .red
+        hud.addChild(self.scoreNode)
+    }
+
+}
